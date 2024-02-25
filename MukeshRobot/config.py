b4e3af1f059bd7f03a8ddd601e42afa29754b96f
Lakshay3434 <156693916+Lakshay3434@users.noreply.github.com>
2024-02-25 09:39:28 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -4,13 +4,13 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID = "29230755" # integer value, dont use ""
+    API_HASH = "ab41c7247a91680d2d0dc705ad7158da"
+    TOKEN = "6924632975:AAEgZTprCgNsM8NlLIYRthZHNnDuJugoaY4"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 6440363814 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
-    START_IMG = ""
+    SUPPORT_CHAT = "Devine_Support"
+    START_IMG = "https://graph.org/file/ab6e5b8f40192031e68d7.jpg"
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
     # RECOMMENDED
@@ -18,11 +18,10 @@ class Config(object):
     CASH_API_KEY = (
         ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = "JGGE4IGZIV2EH339"
     
     # Get your API key from https://timezonedb.com/api
-    API_KEY=""
-    #generate api from telegram using /token command bot username :>> @adventure_robot
+    API_KEY="OGLUXI6U9XIS"
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
